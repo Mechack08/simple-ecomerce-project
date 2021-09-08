f735e3d556fb1e78f2da994928b8c979ff86d2e7
8pro <mechackkobius@gmail.com>
2021-09-08 14:45:57 +0300

    register page completed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Home from "./pages/Home";
 import "./style.css";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Login />
     </div>
   );
 }
